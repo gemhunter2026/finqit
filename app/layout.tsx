060e5af6b496fcdc89c3cf311faef52adc98e5bb
gemhunter2026 <gemhunter2026@proton.me>
2026-09-15 22:12:32 +0200

    Load marketplace product-depth styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./marketplace.css";
+import "./product-depth.css";
 import { Header } from "@/components/Header";
 import { MobileNav } from "@/components/MobileNav";
 
